a98a52687af65225efddff3567c05a67e562aca0
jliriano <jd.liriano@alumnos.upm.es>
2019-03-14 17:19:08 +0100

    fixing code smell - issue #38

--- src/main/java/es/upm/miw/business_controllers/TicketController.java
+++ src/main/java/es/upm/miw/business_controllers/TicketController.java
@@ -12,7 +12,6 @@ import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
