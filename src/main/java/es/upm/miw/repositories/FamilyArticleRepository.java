9e97d7727310ff4453aa01985e171e18ddd603bf
Bruno Martin <=>
2019-03-14 20:49:46 +0100

    add findAllFamilyCompositeByFamilyType to FamilyCompositeRepository #46

--- src/main/java/es/upm/miw/repositories/FamilyArticleRepository.java
+++ src/main/java/es/upm/miw/repositories/FamilyArticleRepository.java
@@ -1,13 +1,8 @@
 package es.upm.miw.repositories;
 
 import es.upm.miw.documents.FamilyArticle;
-import es.upm.miw.documents.FamilyType;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface FamilyArticleRepository extends MongoRepository<FamilyArticle, String> {
-
-    List<FamilyArticle> findByFamilyType (FamilyType familyType);
 
 }
