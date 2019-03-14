256f3d59720fece026456e5168717da46670ee4d
Bruno Martin <=>
2019-03-14 20:58:59 +0100

    Initial commit #36

--- src/main/java/es/upm/miw/repositories/FamilyArticleRepository.java
+++ src/main/java/es/upm/miw/repositories/FamilyArticleRepository.java
@@ -4,5 +4,4 @@ import es.upm.miw.documents.FamilyArticle;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface FamilyArticleRepository extends MongoRepository<FamilyArticle, String> {
-
 }
--- src/test/java/es/upm/miw/repositories/ArticlesFamilyRepositoryIT.java
+++ src/test/java/es/upm/miw/repositories/ArticlesFamilyRepositoryIT.java
@@ -82,4 +82,8 @@ class ArticlesFamilyRepositoryIT {
         this.familyArticleRepository.deleteAll();
     }
 
+    @Test
+    void testFindAllFamilyCompositeByFamilyType (){
+
+    }
 }
