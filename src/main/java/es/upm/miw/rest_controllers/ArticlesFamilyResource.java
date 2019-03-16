21f156c4d877667988e97db2b169abbc6915d99f
Bruno Martin <=>
2019-03-16 13:51:32 +0100

    added ArticlesFamilyResource #52

--- src/main/java/es/upm/miw/rest_controllers/ArticlesFamilyResource.java
+++ src/main/java/es/upm/miw/rest_controllers/ArticlesFamilyResource.java
@@ -1,4 +1,30 @@
 package es.upm.miw.rest_controllers;
 
+import es.upm.miw.business_controllers.ArticlesFamilyController;
+import es.upm.miw.documents.FamilyType;
+import es.upm.miw.dtos.ArticleFamilyMinimumDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('OPERATOR')")
+@RestController
+@RequestMapping(ArticlesFamilyResource.ARTICLES_FAMILY)
 public class ArticlesFamilyResource {
+
+    public static final String ARTICLES_FAMILY = "/articles-family";
+
+    @Autowired
+    private ArticlesFamilyController articlesFamilyController;
+
+    @GetMapping
+    public List<ArticleFamilyMinimumDto> readAllFamilyCompositeByFamilyType(@Valid @PathVariable FamilyType familyType){
+        return articlesFamilyController.readAllFamilyCompositeByFamilyType(familyType);
+    }
 }
