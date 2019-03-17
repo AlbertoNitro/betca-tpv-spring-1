bde5182d40c8f2c09cd5525ac4e4365cc050a76e
Tomás <tomini18@hotmail.com>
2019-03-17 23:02:26 +0100

    Created Statistics REST Controller. issue #67

---
+++ src/main/java/es/upm/miw/rest_controllers/StatisticResource.java
@@ -0,0 +1,40 @@
+package es.upm.miw.rest_controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.upm.miw.business_controllers.StatisticController;
+import es.upm.miw.dtos.StatisticDtoOutput;
+
+@PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER')")
+@RestController
+@RequestMapping(StatisticResource.STATISTIC)
+public class StatisticResource {
+
+    public static final String STATISTIC = "/statistics";
+
+    public static final String STATISTIC_NAME = "/{statisticName}";
+
+    @Autowired
+    private StatisticController statisticController;
+
+    @PreAuthorize("authenticated")
+    @GetMapping(value = STATISTIC_NAME)
+    public List<StatisticDtoOutput> read(@PathVariable String statisticName, @RequestParam String dateFrom, @RequestParam String dateTo) {
+        List<StatisticDtoOutput> dataReturn = new ArrayList<>();
+        if (statisticName.equalsIgnoreCase("totalSalesPerDay")) {
+            dataReturn = this.statisticController.getDataStatisticTotalSalesPerDay(dateFrom, dateTo);
+        } else if (statisticName.equalsIgnoreCase("averageDailyExpense")) {
+            dataReturn = this.statisticController.getDataStatisticAverageDailyExpense(dateFrom, dateTo);
+        }
+        return dataReturn;
+    }
+}
