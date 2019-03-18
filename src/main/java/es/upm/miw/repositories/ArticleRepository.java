2252a8e1b2d88e4e8b57451bd5c7eacf86ce8eb4
Alex <alejandro.delpuerto2@gmail.com>
2019-03-18 18:23:54 +0100

    Add Article Search by Description, controllers and tests issue #72

--- src/main/java/es/upm/miw/repositories/ArticleRepository.java
+++ src/main/java/es/upm/miw/repositories/ArticleRepository.java
@@ -1,8 +1,14 @@
 package es.upm.miw.repositories;
 
 import es.upm.miw.documents.Article;
+import es.upm.miw.dtos.ArticleSearchDto;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface ArticleRepository extends MongoRepository<Article, String> {
 
+    @Query("?#{ [0] == null ? { $where : 'true'} : { description : {$regex:[0], $options: 'i'} } }")
+    List<ArticleSearchDto> findByDescriptionLikeIgnoreCaseNullSafe(String description);
 }
--- src/main/java/es/upm/miw/rest_controllers/ArticleResource.java
+++ src/main/java/es/upm/miw/rest_controllers/ArticleResource.java
@@ -2,12 +2,14 @@ package es.upm.miw.rest_controllers;
 
 import es.upm.miw.business_controllers.ArticleController;
 import es.upm.miw.dtos.ArticleDto;
+import es.upm.miw.dtos.ArticleSearchDto;
 import es.upm.miw.dtos.input.FamilySizeInputDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('OPERATOR')")
 @RestController
@@ -19,6 +21,8 @@ public class ArticleResource {
     public static final String CODE_ID = "/{code}";
 
     public static final String FAMILY_SIZE = "/family-size";
+
+    public static final String QUERY = "/query";
 
     @Autowired
     private ArticleController articleController;
@@ -36,6 +40,11 @@ public class ArticleResource {
     @PostMapping(value = FAMILY_SIZE)
     public FamilySizeInputDto createFamilySize(@Valid @RequestBody FamilySizeInputDto familySizeInputDto) {
         return this.articleController.createFamilySize(familySizeInputDto);
+    }
+
+    @GetMapping(value = QUERY)
+    public List<ArticleSearchDto> readArticles(@RequestBody String description) {
+        return this.articleController.readArticles(description);
     }
 
 
