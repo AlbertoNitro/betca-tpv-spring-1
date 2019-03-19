3f8f2be86e4f999cf513c86ed9470ce443fa5be1
Bruno Martin <=>
2019-03-19 20:03:36 +0100

    added FamilyCompositeInputDto #78

--- src/main/java/es/upm/miw/dtos/input/FamilyCompositeInputDto.java
+++ src/main/java/es/upm/miw/dtos/input/FamilyCompositeInputDto.java
@@ -1,4 +1,52 @@
 package es.upm.miw.dtos.input;
 
+import es.upm.miw.documents.FamilyType;
+
+import javax.validation.constraints.NotNull;
+
 public class FamilyCompositeInputDto {
+
+    @NotNull
+    private FamilyType familyType;
+
+    private String reference;
+
+    @NotNull
+    private String description;
+
+    public FamilyCompositeInputDto() {
+    }
+
+    public FamilyType getFamilyType() {
+        return familyType;
+    }
+
+    public void setFamilyType(FamilyType familyType) {
+        this.familyType = familyType;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "FamilyCompositeInputDto{" +
+                "familyType=" + familyType +
+                ", reference='" + reference + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
