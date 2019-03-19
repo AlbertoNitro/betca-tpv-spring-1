51f9206ba60d43a8803f7c657910e44d64cb5ec0
Tomás <tomini18@hotmail.com>
2019-03-19 18:40:04 +0100

    completed test. issue #75

--- src/test/java/es/upm/miw/business_controllers/StatisticsControllerIT.java
+++ src/test/java/es/upm/miw/business_controllers/StatisticsControllerIT.java
@@ -1,15 +1,12 @@
 package es.upm.miw.business_controllers;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import es.upm.miw.TestConfig;
-import es.upm.miw.dtos.ArticleDto;
 import es.upm.miw.dtos.output.StatisticDtoOutput;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -20,19 +17,23 @@ class StatisticsControllerIT {
     @Autowired
     private StatisticController statisticController;
 
-    private ArticleDto articleDto;
+    @Test
+    void testGetDataTotalSalesPerDayForStatistics() {
+        LocalDateTime dateFrom = LocalDateTime.now().minusDays(1).withNano(0);
+        LocalDateTime dateTo = LocalDateTime.now().plusDays(1).withNano(0);
 
-    @BeforeEach
-    void seed() {
-        this.articleDto = new ArticleDto("non exist", "descrip", "ref", BigDecimal.TEN, null);
+        List<StatisticDtoOutput> statisticDtoOutputList = this.statisticController
+                .getDataStatisticTotalSalesPerDay(dateFrom.toString(), dateTo.toString());
+        assertTrue(statisticDtoOutputList.size() > 0);
     }
 
     @Test
-    void testGetDataTotalSalesPerDayForStatistics() {
-        LocalDateTime dateFrom = LocalDateTime.now().minusDays(1);
-        LocalDateTime dateTo = LocalDateTime.now().plusDays(1);
+    void testGetAverageDailyExpenseForStatistics() {
+        LocalDateTime dateFrom = LocalDateTime.now().minusDays(1).withNano(0);
+        LocalDateTime dateTo = LocalDateTime.now().plusDays(1).withNano(0);
 
-        List<StatisticDtoOutput> statisticDtoOutputList = this.statisticController.getDataStatisticTotalSalesPerDay(dateFrom.toLocalDate().toString(), dateTo.toLocalDate().toString());
+        List<StatisticDtoOutput> statisticDtoOutputList = this.statisticController
+                .getDataStatisticAverageDailyExpense(dateFrom.toString(), dateTo.toString());
         assertTrue(statisticDtoOutputList.size() > 0);
     }
 }
