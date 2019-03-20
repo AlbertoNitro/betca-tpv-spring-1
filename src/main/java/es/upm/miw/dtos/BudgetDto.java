2ee12db01dc17dc352a8763ebb7d6d8e1c2cbc55
pmoreno <pmorenopina@gmail.com>
2019-03-20 20:06:03 +0100

    Added setters & getters to budgetDTO #85

--- src/main/java/es/upm/miw/dtos/BudgetDto.java
+++ src/main/java/es/upm/miw/dtos/BudgetDto.java
@@ -22,4 +22,28 @@ public class BudgetDto {
         this.creationDate = budget.getCreationDate();
         this.shoppingList = budget.getShoppingList();
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Shopping[] getShoppingList() {
+        return shoppingList;
+    }
+
+    public void setShoppingList(Shopping[] shoppingList) {
+        this.shoppingList = shoppingList;
+    }
 }
