9357d5403aef81ff2bce245f81a42b0f22519a1e
Jose Lorenzo <jolorenzom@gmail.com>
2019-03-24 12:17:06 +0100

    Rollback OfferResource (quit POST /offers endpoint) #106

--- src/main/java/es/upm/miw/rest_controllers/OfferResource.java
+++ src/main/java/es/upm/miw/rest_controllers/OfferResource.java
@@ -1,10 +1,7 @@
 package es.upm.miw.rest_controllers;
 
-import es.upm.miw.dtos.input.OfferInputDto;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('OPERATOR')")
 @RestController
@@ -16,10 +13,5 @@ public class OfferResource {
     @GetMapping
     public String readAll() {
         return "Entra a /offers";
-    }
-
-    @PostMapping
-    public OfferInputDto createOffer(@Valid @RequestBody OfferInputDto offerInputDto) {
-        return offerInputDto;
     }
 }
