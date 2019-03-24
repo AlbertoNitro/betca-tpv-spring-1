6abbc48b7ec870df6b63a7ca234cb5fc1f2d3042
Jose Lorenzo <jolorenzom@gmail.com>
2019-03-24 12:48:15 +0100

    POST /offers endpoint. return body #109

--- src/main/java/es/upm/miw/rest_controllers/OfferResource.java
+++ src/main/java/es/upm/miw/rest_controllers/OfferResource.java
@@ -1,7 +1,10 @@
 package es.upm.miw.rest_controllers;
 
+import es.upm.miw.dtos.input.OfferInputDto;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('OPERATOR')")
 @RestController
@@ -14,4 +17,11 @@ public class OfferResource {
     public String readAll() {
         return "Entra a /offers";
     }
+
+    @PostMapping
+    public OfferInputDto createOffer(@Valid @RequestBody OfferInputDto offerInputDto) {
+        return offerInputDto;
+    }
+
+
 }
