0c01ad973f6fcdc0863a66a6ba70f919577bc8cd
Dennys <dennys.landy@hotmail.com>
2019-03-26 11:44:12 +0100

    REST AlarmResource Controller, first version #81

--- src/main/java/es/upm/miw/rest_controllers/AlarmResource.java
+++ src/main/java/es/upm/miw/rest_controllers/AlarmResource.java
@@ -1,4 +1,17 @@
 package es.upm.miw.rest_controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (AlarmResource.ALARMS)
 public class AlarmResource {
+
+    public static final String ALARMS = "/alarms";
+
+
+    @GetMapping()
+    public String readAll() { return "Test"; };
+
 }
