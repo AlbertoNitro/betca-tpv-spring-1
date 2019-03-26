1ca6713137346d9954d47243273b92d5eda8e452
pmoreno <pmorenopina@gmail.com>
2019-03-26 21:23:36 +0100

    Added test for BudgetResource #88

--- src/test/java/es/upm/miw/rest_controllers/BudgetResourceIT.java
+++ src/test/java/es/upm/miw/rest_controllers/BudgetResourceIT.java
@@ -1,6 +1,7 @@
 package es.upm.miw.rest_controllers;
 
 import es.upm.miw.dtos.BudgetDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,6 +16,18 @@ class BudgetResourceIT {
     @Autowired
     private RestService restService;
 
+    private BudgetDto existentBudget;
+
+
+    @BeforeEach
+    void before() {
+        List<BudgetDto> budgets = Arrays.asList(this.restService.loginAdmin()
+                .restBuilder(new RestBuilder<BudgetDto[]>()).clazz(BudgetDto[].class)
+                .path(BudgetResource.BUDGETS)
+                .get().build());
+        this.existentBudget = budgets.get(0);
+    }
+
     @Test
     void testReadAll() {
         List<BudgetDto> budgetDtoList = Arrays.asList(this.restService.loginAdmin()
@@ -22,6 +35,18 @@ class BudgetResourceIT {
                 .path(BudgetResource.BUDGETS)
                 .get().build());
         assertTrue(budgetDtoList.size() > 0);
+    }
+
+    @Test
+    void testReadById() {
+        BudgetDto budgetDto = this.restService.loginAdmin()
+                .restBuilder(new RestBuilder<BudgetDto>()).clazz(BudgetDto.class)
+                .path(BudgetResource.BUDGETS)
+                .path(BudgetResource.ID)
+                .expand(this.existentBudget.getId())
+                .get().build();
+        assertNotNull(budgetDto);
+        assertNotNull(budgetDto.getId());
     }
 
 }
