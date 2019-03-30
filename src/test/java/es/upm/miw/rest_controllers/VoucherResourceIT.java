3ff9574b875c59cdc2894a0c86063b630957590b
ropili19 <rosariopili26@gmail.com>
2019-03-30 17:26:44 +0100

    update VoucherResourceIT

--- src/test/java/es/upm/miw/rest_controllers/VoucherResourceIT.java
+++ src/test/java/es/upm/miw/rest_controllers/VoucherResourceIT.java
@@ -7,9 +7,12 @@ import es.upm.miw.dtos.input.VoucherInputDto;
 import es.upm.miw.dtos.output.VoucherOutputDto;
 import es.upm.miw.repositories.VoucherRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,7 +20,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
 @ApiTestConfig
 class VoucherResourceIT {
@@ -62,6 +67,31 @@ class VoucherResourceIT {
                 .get().build());
         assertTrue(voucherDtoList.size() > 0);
     }
+
+    @Test
+    void testUpdate() {
+        String code = "1234567890";
+        VoucherOutputDto result = restUpdateBuilder(code).build();
+        assertTrue(code.equals(result.getId()));
+
+    }
+
+
+    private RestBuilder<VoucherOutputDto> restUpdateBuilder(String id) {
+        return this.restService.loginAdmin()
+                .restBuilder(new RestBuilder<VoucherOutputDto>()).clazz(VoucherOutputDto.class)
+                .path(VoucherResource.VOUCHERS).path("/" + id)
+                .put();
+    }
+
+    private RestBuilder<VoucherOutputDto> restCreateService(VoucherOutputDto voucherDto) {
+        return this.restService.loginAdmin()
+                .restBuilder(new RestBuilder<VoucherOutputDto>()).clazz(VoucherOutputDto.class)
+                .path(VoucherResource.VOUCHERS)
+                .body(voucherDto)
+                .post();
+    }
+
 
 
 }
