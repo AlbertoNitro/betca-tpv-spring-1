035b3e79c55c839268a6d007abbbce45ceac23fb
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2019-03-31 16:44:25 +0200

    added discontinued in builder #175

--- src/main/java/es/upm/miw/documents/Article.java
+++ src/main/java/es/upm/miw/documents/Article.java
@@ -177,6 +177,11 @@ public class Article {
             return this;
         }
 
+        public Builder discontinued(Boolean discontinued) {
+            this.article.discontinued = discontinued;
+            return this;
+        }
+
         public Article build() {
             return this.article;
         }
