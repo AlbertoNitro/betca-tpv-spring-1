2946a5ad6f1ba2e1cc6fae8530eee461499fb1e5
manupuchades <m.puchades@gmail.com>
2019-03-31 22:21:41 +0200

    Gift Ticket Pdf functionality + refactoring issue #101

--- src/main/java/es/upm/miw/business_services/PdfService.java
+++ src/main/java/es/upm/miw/business_services/PdfService.java
@@ -75,7 +75,7 @@ public class PdfService {
 
     private int generateTableCellFromShopping(PdfTableBuilder table, Shopping shopping, int index, int notCommitted) {
         String state = "";
-        if (shopping.getShoppingState() != ShoppingState.COMMITTED && shopping.getAmount() > 0) {
+        if (isShoppingNotCommitted(shopping)) {
             state = "N";
             notCommitted++;
         }
@@ -148,35 +148,48 @@ public class PdfService {
         final String path = "/tpv-pdfs/tickets/ticket-" + ticket.getId();
         PdfBuilder pdf = new PdfBuilder(path);
         this.generateCommonHead(pdf);
-        if (ticket.isDebt()) {
-            pdf.paragraphEmphasized("RESERVA");
-            pdf.paragraphEmphasized("Abonado: " + ticket.pay().setScale(2, RoundingMode.HALF_UP) + "€");
-            pdf.paragraphEmphasized("Pendiente: " + ticket.debt().setScale(2, RoundingMode.HALF_UP) + "€");
-        } else {
-            pdf.paragraphEmphasized("TICKET");
-        }
-        pdf.barCode(ticket.getId());
+
+        pdf.paragraphEmphasized("GIFT TICKET");
+
+        pdf.barCode(ticket.getGiftTicket().getId());
         pdf.line();
-        pdf.paragraphEmphasized(ticket.getCreationDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        pdf.paragraphEmphasized("Fecha de emisión: " + ticket.getGiftTicket().getCreationDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        pdf.paragraphEmphasized("Fecha de expiración: " + ticket.getGiftTicket().getExpirationDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
         int notCommitted = 0;
-        PdfTableBuilder table = pdf.table(TABLE_COLUMNS_SIZES_TICKETS).tableColumnsHeader(TABLE_COLUMNS_HEADERS);
+
+        PdfTableBuilder table = pdf.table(TABLE_GIFT_TICKET_COLUMNS_SIZES_TICKETS).tableColumnsHeader(TABLE_GIFT_TICKET_COLUMNS_HEADERS);
         for (int i = 0; i < ticket.getShoppingList().length; i++) {
             Shopping shopping = ticket.getShoppingList()[i];
-            notCommitted = this.generateTableCellFromShopping(table, shopping, i, notCommitted);
+            String state = "";
+            if (isShoppingNotCommitted(shopping)) {
+                state = "N";
+                notCommitted++;
+            }
+            table.tableCell(String.valueOf(i + 1), shopping.getDescription(), "" + shopping.getAmount(), state);
         }
-        table.tableColspanRight(ticket.getTotal().setScale(2, RoundingMode.HALF_UP) + "€").build();
-        pdf.paragraph(ticket.getNote());
+        pdf.paragraph(ticket.getGiftTicket().getNote());
+
         if (notCommitted > 0) {
             pdf.paragraphEmphasized("Artículos pendientes de entrega: " + notCommitted);
             if (ticket.getUser() != null) {
-                pdf.paragraph("Teléfono de contacto: " + ticket.getUser().getMobile() + " - " + ticket.getUser().getUsername().substring(0,
-                        (ticket.getUser().getUsername().length() > 10) ? 10 : ticket.getUser().getUsername().length()));
+                addContactData(pdf, ticket.getUser());
             }
             pdf.qrCode(ticket.getReference());
         }
+
         pdf.line().paragraph("Periodo de devolución o cambio: 15 dias a partir de la fecha del ticket");
         this.generateCommonFooter(pdf);
         return pdf.build();
+    }
+
+    private boolean isShoppingNotCommitted(Shopping shopping){
+        return shopping.getShoppingState() != ShoppingState.COMMITTED && shopping.getAmount() > 0;
+    }
+
+    public void addContactData (PdfBuilder pdf, User user){
+        pdf.paragraph("Teléfono de contacto: " + user.getMobile() + " - " + user.getUsername().substring(0,
+                (user.getUsername().length() > 10) ? 10 : user.getUsername().length()));
     }
 
     public byte[] generatePrintableRgpdAgreement(User user, RgpdAgreementType type) {
